8889bf6174f4191f065d66e05d47cfd75b62b486
Safwan202107146 <118248342+Safwan202107146@users.noreply.github.com>
2025-03-10 03:47:21 +0530

    Add files via upload

---
+++ SPRINT-2/TASK-3/script.js
@@ -0,0 +1,56 @@
+// ***** EX-1 ******//
+
+function sayHello(name) {
+     console.log(`Hello ${name}!`);
+}
+sayHello("Safwan")
+
+
+// ***** EX-2 ******//
+
+function add(a,b) {
+    return (a+b);
+}
+console.log("Sum of Two parameters :",add(10,20))
+
+
+// ***** EX-3 ******//
+
+function mul(x,y) {
+    return (x*y);
+}
+console.log("Product of 5*10 :",mul(5,10))
+console.log("Product of 10*10 :",mul(10,10))
+
+// ***** EX-4 ******//
+
+function mul(x,y) {
+    return (x*y);
+}
+console.log("Product of Two parameters :",mul(10,20))
+
+// ***** EX-5 ******//
+
+function div(x,y) {
+    return (x/y);
+}
+console.log("Divide 10 by 5 :",div(10,5))
+
+
+// ***** EX-6 ******//
+
+function fact(n){
+    return n*(n-1);
+}
+console.log("Factorial of 10 :",fact(10))
+console.log("Factorial of 15 :",fact(15))
+
+
+// ***** EX-7 ******//
+
+function square(n){
+    return n*n;
+}
+console.log("Square of 10 :",square(10))
+console.log("Square of 15 :",square(15))
+
